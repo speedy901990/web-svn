77b0d265281c0b0999ed5761cf5a31c5490e4738
speedy <speedy@89.76.245.182>
2013-02-26 23:38:55 +0100

    Default Rails generated project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebSvn::Application.config.secret_token = '00e897af6468bb48d00022bbb43f6a964d4dc2cf4d3b7c0fa6c8671ee5b422e0c9bd704f3ddafcf3746c4152f70228a645933eb3d7d84e6629ad13a6afc63fcd'

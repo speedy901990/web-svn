e0275d2c416eecffa53733010446cb0d653be8bf
speedy901990 <mychaty@gmail.com>
2013-04-08 14:57:04 +0200

    ok

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -2,18 +2,20 @@ class RepositoriesController < ApplicationController
   def new
     @repository = Repository.new
   end
-  def create
-    
+  def create    
     @repository = Repository.new(params[:repository])
     if @repository.save
       #par = params[:repository]
       par = params[:repository][:repo_name]
       @result = %x[script/create_repo.sh #{par}]
-      redirect_to root_url
+      redirect_to :controller => 'home', :action => 'index'
     else
       render "new"
     end
   end
   def delete_repo
+    #@items = Repository.find(params[:id])#.return_items
+    #@repository = Repository.find(params[:id])
+    render "delete_repo"
   end
 end
--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,3 +1,7 @@
 class Repository < ActiveRecord::Base
   attr_accessible :repo_name
+  
+  def self.return_items
+    find(:all)
+  end
 end

d5e13a9c4397bca8d72824854924b211499b3b4c
Slawek Kwasniewski <slk1871@gmail.com>
2013-04-22 20:23:58 +0200

    Added views for groups and grouptorepo

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -16,8 +16,46 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
     @group.destroy
     respond_to do |format|	
-      format.html { redirect_to repositories_path }
+      format.html { redirect_to groups_path }
       format.json { head :no_content }
     end
   end
+
+  def index
+    @group = Group.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @group }
+    end
+  end
+
+def edit
+    @group = Group.find(params[:id])
+  end
+
+  def show
+    @group = Group.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @group }
+    end
+  end
+  
+  def update
+    @group = Group.find(params[:id])
+
+    respond_to do |format|
+      if @group.update_attributes(params[:group])
+        format.html { redirect_to groups_path, notice: 'Group was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @group.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
 end
--- app/controllers/grouptorepos_controller.rb
+++ app/controllers/grouptorepos_controller.rb
@@ -16,11 +16,48 @@ class GrouptoreposController < ApplicationController
   end
 
  def destroy    
-   @repotogroup = Grouptorepo.find(params[:repository_id])
-   @repotogroup.destroy
+   @grouptorepo = Grouptorepo.find(params[:repository_id])
+   @grouptorepo.destroy
    respond_to do |format|	
      format.html { redirect_to repositories_path }
      format.json { head :no_content }
    end
  end
+
+def index
+    @grouptorepo = Grouptorepo.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @grouptorepo }
+    end
+  end
+
+ def edit
+    @grouptorepo = Grouptorepo.find(params[:id])
+  end
+
+  def show
+    @grouptorepo = Grouptorepo.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @grouptorepo }
+    end
+  end
+  
+  def update
+    @grouptorepo = Grouptorepo.find(params[:id])
+
+    respond_to do |format|
+      if @grouptorepo.update_attributes(params[:repository])
+        format.html { redirect_to grouptorepos_path, notice: 'Grouptorepo was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @grouptorepo.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
 end

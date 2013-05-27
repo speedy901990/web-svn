18c3eb3e3200c06960ccf1bca683b6b9f722409a
speedy <speedy@thinkpad>
2013-05-27 22:50:47 +0200

    first security

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     user = User.authenticate(params[:email], params[:password])
     if user
       session[:user_id] = user.id
+      session[:user_is_admin] = user.is_admin
       redirect_to root_url, :notice => "Logged in!"
     else
       flash.now.alert = "Invalid email or password"
@@ -15,6 +16,7 @@ class SessionsController < ApplicationController
   
   def destroy
     session[:user_id] = nil
+    session[:user_is_admin] = nil
     #flash.now[:notice] = "TEST MESSAGE"
     redirect_to root_url, :notice => "Logged out!"
     

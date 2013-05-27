bb9ef76dad3d2d4e523de9e7332d2bf3ccaee856
Slawek Kwasniewski <slk1871@gmail.com>
2013-05-28 00:06:53 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 
 WebSvn::Application.routes.draw do
+
+  root :to => "home#index"
+
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "log_in" => "sessions#new", :as => "log_in"
   get "sign_up" => "users#new", :as => "sign_up"

bf323490f0796e2a99cf5ff3b60df4650ba978f8
Slawek Kwasniewski <slk1871@gmail.com>
2013-06-03 23:03:23 +0200

    something is wrong with notifications in user_show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,9 +50,12 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @gtr = Usertogroup.where("user_id = ?", params[:id]).select("group_id").first
+
+    @repositories = Usermailinglist.where('user_id = ?', params[:id]).select("repo_id").all
     if @gtr != nil
     @x = Group.where("id = ?", @gtr.group_id).select("group_name").first
     end
+    
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @user }
